eed4d21846ce8293b5c458262c949237bb982540
rmcbrid5 <rmcbrid5@uwo.ca>
2019-02-12 17:25:11 -0500

    Added static data api requests

--- league-match-history/src/App.js
+++ league-match-history/src/App.js
@@ -6,8 +6,13 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      value: ''
+      value: '',
+      summoners: [],
+      items: [],
+      champions: []
     }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
   handleChange(event){
     this.setState({
@@ -16,6 +21,54 @@ class App extends Component {
   }
   handleSubmit(event){
     event.preventDefault();
+    //static api call to fetch all the summoner spells
+    fetch("http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/summoner.json")
+            .then(res => res.json())
+            .then(
+            (result) => {
+                for (var j in result.data) {
+                  //data has unique property names
+                    if (result.data.hasOwnProperty(j)) {
+                        this.state.summoners.push({
+                            key: result.data[j].key,
+                            id: result.data[j].id,
+                            name: result.data[j].name
+                        })
+                    }
+                }
+            })
+    //static data api call to fetch the game items
+    fetch("http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/item.json")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        for (var j in result.data) {
+          // each game item has a unique property
+          if (result.data.hasOwnProperty(j)) {
+            var id = result.data[j].image.full;
+            id = id.toString().slice(0, 4);
+            this.state.items.push({
+              key: id,
+              name: result.data[j].name
+            })
+          }
+        }
+      })
+      //static data api call to fetch the champions
+        fetch("http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    for (var j in result.data) {
+                      //each champion has a unique property
+                        if (result.data.hasOwnProperty(j)) {
+                            this.state.champions.push({
+                                key: result.data[j].key,
+                                name: result.data[j].name
+                            })
+                        }
+                    }
+                })
   }
   render() {
     return (
