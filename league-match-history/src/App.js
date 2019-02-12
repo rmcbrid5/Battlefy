e4137504c511659d0ce38e9e8b6cea85f7e60e22
rmcbrid5 <rmcbrid5@uwo.ca>
2019-02-12 17:13:25 -0500

    created basic layout for the search function

--- league-match-history/src/App.js
+++ league-match-history/src/App.js
@@ -3,22 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value: ''
+    }
+  }
+  handleChange(event){
+    this.setState({
+      value: event.target.value
+    })
+  }
+  handleSubmit(event){
+    event.preventDefault();
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <form onSubmit={this.handleSubmit} >
+        <img src="https://upload.wikimedia.org/wikipedia/en/7/77/League_of_Legends_logo.png"/>
+        <h1>SUMMONER HISTORY LOOKUP</h1>
+        <label>
+          Summoner Name:
+          <input className="searchName" type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+          </label>
+          <input className="search" type="submit" value="Search" />
+          </form>
         </header>
       </div>
     );
